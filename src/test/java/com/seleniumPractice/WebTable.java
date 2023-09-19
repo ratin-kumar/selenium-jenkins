5eb8ed9b1360c16ec8c6b0df0e8f74d7f4eed445
Ratin Kumar <kumarratin@gmail.com>
2023-09-19 22:16:44 +0530

    Parameter added

--- src/test/java/com/seleniumPractice/WebTable.java
+++ src/test/java/com/seleniumPractice/WebTable.java
@@ -2,60 +2,72 @@ package com.seleniumPractice;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class WebTable {
-	
+
 	// Get all data from the Web Table
-	
-	WebDriver driver = new FirefoxDriver();
-	
+
+	WebDriver driver = null;
+
+	@Parameters("Browser")
 	@BeforeTest
-	public void setUp() throws InterruptedException {	
+	public void setUp(String browser) throws InterruptedException {
+
+		if (browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (browser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		}
 		driver.get("https://www.w3schools.com/html/html_tables.asp");
 		driver.manage().window().maximize();
 		Thread.sleep(3000);
 
 	}
-	
+
 	@Test
 	public void getWebTableData() {
-		
+
 		int rowCount = driver.findElements(By.xpath("//table[@class='ws-table-all']/tbody/tr")).size();
 		int colCount = driver.findElements(By.xpath("//table[@class='ws-table-all']/tbody/tr[2]/td")).size();
 		String xpath1 = "//table[@class='ws-table-all']/tbody/tr[";
 		String xpath2 = "]/td[";
 		String xpath3 = "]";
-		
-		System.out.println("Row: "+ rowCount);
-		System.out.println("Col: "+ colCount);
-		
-		for(int i=2; i<=rowCount; i++) {
-			for(int j=1; j<=colCount; j++) {
-				String tableText = driver.findElement(By.xpath(xpath1+i+xpath2+j+xpath3)).getText();
-				System.out.print(tableText+ ",  ");
+
+		System.out.println("Row: " + rowCount);
+		System.out.println("Col: " + colCount);
+
+		for (int i = 2; i <= rowCount; i++) {
+			for (int j = 1; j <= colCount; j++) {
+				String tableText = driver.findElement(By.xpath(xpath1 + i + xpath2 + j + xpath3)).getText();
+				System.out.print(tableText + ",  ");
 			}
 			System.out.println();
 		}
-		
+
 	}
-	
+
 	@Test
 	public void getWebTableData2() {
-		
+
 		int row = 2;
 		int col = 3;
 		int data = 2;
-		
+
 		String xpath1 = "//table[@class='ws-table-all']/tbody/tr[";
 		String xpath2 = "]/td[";
 		String xpath3 = "]";
-		
-		for(int i=row; i<=row+data; i++) {
-			String text = driver.findElement(By.xpath(xpath1+i+xpath2+col+xpath3)).getText();
+
+		for (int i = row; i <= row + data; i++) {
+			String text = driver.findElement(By.xpath(xpath1 + i + xpath2 + col + xpath3)).getText();
 			System.out.println(text);
 		}
 	}
